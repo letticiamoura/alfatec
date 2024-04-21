6b9dfb31564f63c5807bcaccf8f5eff1d44164bf
Leticia-Lopes2 <leticiamouralopes5@gmail.com>
2024-04-21 08:52:15 -0300

    style: Alterando cor dos card

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -14,29 +14,27 @@ export default function About() {
 
             <div className="pt-5">
 
-                <h2 className="text-lg">Somos uma renomada rede de ensino especializada em capacitar profissionais para atender às demandas do mercado de trabalho atual. Oferecemos uma variedade de cursos técnicos e profissionalizantes para garantir que nossos alunos estejam preparados para enfrentar os desafios e se destacarem em suas carreiras.</h2>
+                <h2 className="text-lg font">Somos uma renomada rede de ensino especializada em capacitar profissionais para atender às demandas do mercado de trabalho atual. Oferecemos uma variedade de cursos técnicos e profissionalizantes para garantir que nossos alunos estejam preparados para enfrentar os desafios e se destacarem em suas carreiras.</h2>
 
                 <div className="pt-5 flex flex-col gap-5">
-                    <div className="p-5 border shadow-xl bg-orange-200/80 rounded-tl-[50px] rounded-br-[50px]">
+                    <div className="p-5 border shadow-xl bg-orange-500 rounded-tl-[50px] rounded-br-[50px] text-white">
                         <img src={valores} alt="Valores" className="h-[10vh] m-auto"/>
-                        <h2 className="text-2xl font-bold text-orange-500 text-center">Valores</h2>
-                        <li className="text-lg text-orange-500">Honestidade</li>
-                        <li className="text-lg text-orange-500">Respeito</li>
-                        <li className="text-lg text-orange-500">Compromisso</li>
-                        <li className="text-lg text-orange-500">Ética</li>
+                        <h2 className="text-2xl font-bold text-center">Valores</h2>
+                        <li className="text-lg">Honestidade</li>
+                        <li className="text-lg">Respeito</li>
+                        <li className="text-lg">Compromisso</li>
+                        <li className="text-lg">Ética</li>
                     </div>
 
-
-
-                    <div className="p-5 border bg-orange-200/80  rounded-tl-[50px] rounded-br-[50px] shadow-xl">
+                    <div className="p-5 border bg-orange-500 text-white rounded-tl-[50px] rounded-br-[50px] shadow-xl">
                         <img src={visao} alt="Visão" className="h-[10vh] m-auto"/>
-                        <h2 className="text-2xl font-bold text-orange-500 text-center">Visão</h2>
-                        <p  className="text-lg text-orange-500">Ser a melhor Escola Técnica do Brasil reconhecida como o melhor método de ensino e a mais eficiente na realização dos sonhos de nossos alunos.</p>
+                        <h2 className="text-2xl font-bold text-center">Visão</h2>
+                        <p  className="text-ld">Ser a melhor Escola Técnica do Brasil reconhecida como o melhor método de ensino e a mais eficiente na realização dos sonhos de nossos alunos.</p>
                     </div>
-                    <div className="p-5 border bg-orange-200/80  rounded-tl-[50px] rounded-br-[50px] shadow-xl">
+                    <div className="p-5 border bg-orange-500 text-white rounded-tl-[50px] rounded-br-[50px] shadow-xl">
                         <img src={missao} alt="Missão" className="h-[10vh] m-auto"/>
-                        <h2 className="text-2xl font-bold text-orange-500 text-center">Missão</h2>
-                        <p  className="text-lg text-orange-500">Proporcionar aos alunos ensino de excelência visando desenvolver o potencial de cada educando e formando cidadãos úteis á sociedade e á Pátria.</p>
+                        <h2 className="text-2xl font-bold text-center">Missão</h2>
+                        <p  className="text-lg">Proporcionar aos alunos ensino de excelência visando desenvolver o potencial de cada educando e formando cidadãos úteis á sociedade e á Pátria.</p>
                     </div>
                 </div>
                 
