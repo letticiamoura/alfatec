966cd8846ad8ae7b48ca1bd918659b79e275aefd
Leticia Moura <leeh.mourah62@gmail.com>
2024-11-16 18:27:49 -0300

    feat: configuração de deploy

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import About from "./pages/About";
 
 export default function App() {
   return (
-      <BrowserRouter>
+      <BrowserRouter basename="/alfatec">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/cursos" element={<Courses />} />
