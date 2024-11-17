4eb9848a050d4a39e5ecd86da61f58c46c1de89b
Leticia Moura <leeh.mourah62@gmail.com>
2024-11-17 08:57:08 -0300

    refactor: Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,6 @@ export default function Footer() {
     return (
         <footer className="bg-orange-500 text-white py-5">
             <div className="max-w-7xl mx-auto text-center">
-                <p className="text-lg text-zinc-100">&copy;2024 Todos os direitos reservados</p>
                 <div className="flex justify-center items-center space-x-5 mt-2">
                     <Link to="https://facebook.com.br" target="_blank">
                      <FaFacebook size={30}/>
@@ -17,6 +16,7 @@ export default function Footer() {
                      <FaWhatsapp size={30}/>
                     </Link>
                 </div>
+                <p className="text-lg text-zinc-100">&copy;2024 Todos os direitos reservados</p>
             </div>
         </footer>
     );
